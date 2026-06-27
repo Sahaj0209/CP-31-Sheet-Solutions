d51af2fa2e27b4edba2dc6a92f408b1a293a9369
Sahaj0209 <sahajjaiswal71@gmail.com>
2026-06-27 16:00:27 +0530

    1

---
+++ 900 rated/MakeAP/MakeAP.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long a,b,c;
+        cin>>a>>b>>c;
+        long long newa =(2*b-c);
+        long long newb=(a+c)/2;
+        long long newc=(2*b-a);
+        bool flag = false;
+        if(newa/a>0 && newa%a==0){
+            flag=true;
+        } 
+        if(newc/c > 0 && newc%c==0){
+            flag=true;
+        }
+        if(newb/b>0 && newb%b==0 && (c-a)%2==0){
+            flag=true;
+        }
+        if(flag) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
