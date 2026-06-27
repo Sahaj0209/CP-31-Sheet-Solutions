4536914941a5f6989eb22f81a786ed30e42ebaac
Sahaj0209 <sahajjaiswal71@gmail.com>
2026-06-27 16:50:40 +0530

    1

--- 900 rated/ABBalance/ABBalance.cpp
+++ 900 rated/ABBalance/ABBalance.cpp
@@ -6,8 +6,9 @@ int main(){
     while(t--){
         string s;
         cin>>s;
-        if(s[0]!=s[s.size()-1]){
-            if(s[0]='b'){
+        int n=s.size()-1;
+        if(s[0]!=s[n]){
+            if(s[0]=='b'){
                 s[0]='a';
             } else {
                 s[0]='b';
