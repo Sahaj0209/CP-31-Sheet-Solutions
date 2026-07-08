3109685d65ff1841a5aa4ff6aca99799efe57a8f
Sahaj0209 <sahajjaiswal71@gmail.com>
2026-07-08 13:50:18 +0530

    1

---
+++ 900 rated/BadBoys/BadBoys.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    ll t;cin>>t;
+    while(t--){
+        ll i,j,n,m;cin>>n>>m>>i>>j;
+        cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+    }
+    return 0;
+}
